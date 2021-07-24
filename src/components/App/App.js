f91d11bec509ce7d5ec5df4172ec71a3be213046
Meghan Cleland <mplscleland@gmail.com>
2021-07-23 22:39:22 -0500

    have movie details rendering- need to change it to be specific to selected movie

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import AddMovie from '../AddMovie/AddMovie';
+import MovieDetails from '../MovieDetails/MovieDetails';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         </Route>
         
         {/* Details page */}
-
+        <MovieDetails />
         {/* Add Movie page */}
         <AddMovie />
       </Router>
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,27 @@
-function MovieDetails(){
+// import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from 'react';
 
+function MovieDetails(){
+//movie.description
+const dispatch = useDispatch();
+
+const movies = useSelector(store => store.movies);
+useEffect( () => {
+    dispatch({ type: 'FETCH_MOVIES' });
+    dispatch({ type: 'ADD_DETAILS'})
+}, [])
     return(
         <>
+         {movies.map(movie => {
+                    return (
+                        <div key={movie.id} >
+                            <h3>{movie.description}</h3>
+                        </div>
+                    );
+                })}
         </>
     )
 }
+
+export default MovieDetails;
