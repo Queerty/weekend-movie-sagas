fd9e5a01d7a5b2690a7115fb4dc71b1682665a98
Meghan Cleland <mplscleland@gmail.com>
2021-07-25 15:20:30 -0500

    have movie genres displayed on details view

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -16,19 +16,13 @@ console.log('this is details', details);
         <h1>Movie Details</h1>
         <h2>{details && details[0].title}</h2>
         <img src={details && details[0].poster}/>
-
-
-        {/* {details.map(clickedMovie => {
-            return(
-            
-                <div key={clickedMovie[0].id}>
-                <h2>{clickedMovie[0].title}</h2>
-                <img src={clickedMovie[0].poster} alt={clickedMovie[0].title}/>
-                </div>
-            )
-        })} */}
+        <h2>{details && details[0].description}</h2>
+        <h2>{details && details.map(movieProp =>{
+        return(
+        movieProp.name)})}</h2>
         </>
-    );
-}
+        )}
+
+
 
 export default MovieDetails;
