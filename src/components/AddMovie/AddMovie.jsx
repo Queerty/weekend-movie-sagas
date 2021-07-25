972d5a944515d48059f6a054ff75f88ee174ad4e
Meghan Cleland <mplscleland@gmail.com>
2021-07-24 22:34:07 -0500

    post addmovie is now kinda working

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -3,37 +3,49 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function AddMovie(){
     const dispatch = useDispatch();
-    const [newTitle, setNewTitle] = useState('');
-    const [newPoster, setNewPoster] = useState('');
-    const [newDescription, setNewDescription] = useState('');
+    // const [newTitle, setNewTitle] = useState('');
+    // const [newPoster, setNewPoster] = useState('');
+    // const [newDescription, setNewDescription] = useState('');
+        const genres = useSelector(store => store.genresReducer);
+        const [newMovie, setNewMovie] = useState({
+            title: "",
+            poster: "",
+            description: "",
+            genre_id: ""
+        });
     
-    const handleChange = (event) => {
-        setNewMovie(event.target.value);
+    function handleChange(evt) {
+        const value = evt.target.value;
+        setNewMovie({
+            ...newMovie,
+            [evt.target.name]: value
+
+        });
+        
     }
 
-    const fetchMovies = () => {
-        dispatch({ type: 'FETCH_MOVIES'});
-    }
 
     useEffect(() => {
-        fetchMovies();
+        // dispatch({ type: 'SET_GENRES'})
     }, []);
 
     const handleAddMovie = event => {
         event.preventDefault();
         dispatch({ type: 'ADD_MOVIE', 
-        payload: {title: newTitle, poster: newPoster, description: newDescription}});
+        payload: {title: newMovie.title, poster: newMovie.poster, description: newMovie.description, genre_id: newMovie.genre_id }});
         setNewMovie('');
     }
 
     return(
         <>
         <h2>Add Movie</h2>
-        <input value={newTitle} onChange={handleChange} placeholder="title"/>
-        <input value={newPoster} onChange={handleChange} placeholder="poster"/>
-        <input value={newDescription} onChange={handleChange} placeholder="description"/>
-
-        <button onClick={handleClick}>Add Movie</button>
+        <form>
+        <input type="text" value={newMovie.title} onChange={handleChange} name="title"/>
+        <input type="text" value={newMovie.poster} onChange={handleChange} name="poster"/>
+        <input type="text" value={newMovie.description} onChange={handleChange} name="description"/>
+        <input type="number" value={newMovie.genre_id} onChange={handleChange} name="genre_id"/>
+        <button onClick={handleAddMovie}>Add Movie</button>
+        </form>
         </>
     )
 };
